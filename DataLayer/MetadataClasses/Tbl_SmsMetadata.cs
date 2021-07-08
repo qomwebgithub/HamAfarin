bdceb628245653e9da5c087483b852070b6308a0
Sajad NorMohamadManesh <sajad.normohamadmanesh@gmail.com>
2021-07-08 13:26:25 +0430

    افزودن میتا دیتا جدول اس ام اس

---
+++ DataLayer/MetadataClasses/Tbl_SmsMetadata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DataLayer
+{
+    public class Tbl_SmsMetadata
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "عنوان")]
+        public string Title { get; set; }
+
+        [Display(Name = "متن پیامک")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        [DisplayFormat(DataFormatString = "{0: yyyy/MM/dd}")]
+        public DateTime? EditDate { get; set; }
+
+
+    }
+    [MetadataType(typeof(Tbl_SmsMetadata))]
+    public partial class Tbl_Sms
+    {
+
+    }
+}
