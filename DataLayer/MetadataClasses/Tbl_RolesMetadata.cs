833ae6a0885fe3642f684ec5f0f3c962c0a4d23c
Sajad NorMohamadManesh <sajad.normohamadmanesh@gmail.com>
2021-07-20 14:17:15 +0430

    ساخت متا دیتای نقش برای فارسی سازی عنوان نقش

---
+++ DataLayer/MetadataClasses/Tbl_RolesMetadata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Tbl_RolesMetadata
+    {
+        [Key]
+        public int RoleID { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+        public string RoleSystemName { get; set; }
+
+        [Display(Name = "نقش")]
+        public string RoleDisplayTitle { get; set; }
+    }
+
+    [MetadataType(typeof(Tbl_RolesMetadata))]
+    public partial class Tbl_Roles
+    {
+
+    }
+}
