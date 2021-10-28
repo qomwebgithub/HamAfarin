6c98ebc1a5be610dcf4cd03e1c14986b168a3457
Sajad NorMohamadManesh <sajad.normohamadmanesh@gmail.com>
2021-10-28 17:18:19 +0330

    ساخت ویو مدل برای واریزی به سرمایه گذاران

---
+++ ViewModels/Deposit/Admin/DepositToInvestorsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class DepositToInvestorsViewModel
+    {
+        public List<PlanViewModel> PlansList { get; set; }
+        public List<DepositTypeViewModel> DepositTypesList { get; set; }
+        [Display(Name = "پرداخت شده")]
+        public bool IsPaid { get; set; }
+        [Display(Name = "نام طرح")]
+        [Required(ErrorMessage = "لطفا {0} را انتخاب کنید")]
+        public int Plan_id { get; set; }
+        [Display(Name = "نوع واریز")]
+        [Required(ErrorMessage = "لطفا {0} را انتخاب کنید")]
+        public int DepositType_id { get; set; }
+        [Display(Name = "درصد واریز")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:#.#}")]
+        public decimal YieldPercent { get; set; }
+        [Display(Name = "توضیحات")]
+        public string Description { get; set; }
+        [Display(Name = "تاریخ واریز")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime? DepositDate { get; set; }
+        [Display(Name = "تاریخ واریز")]
+        public string StringDepositDate { get; set; }
+    }
+}
---
+++ ViewModels/Deposit/Admin/InvestorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class InvestorViewModel
+    {
+        public int UserID { get; set; }
+        public long TotalPaymentPrice { get; set; }
+        public long DepositAmount { get; set; }
+        public string MobileNumber { get; set; }
+        public string Shaba { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
