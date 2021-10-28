e0e284ff117652e0dacaa90f2b24176e6f4c0d78
Sajad NorMohamadManesh <sajad.normohamadmanesh@gmail.com>
2021-10-28 17:16:14 +0330

    ساخت متا دیتا برای نوع واریز و واریزی به سرمایه گذاران

---
+++ DataLayer/MetadataClasses/Tbl_DepositToInvestorsMetadata.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DataLayer
+{
+    public class Tbl_DepositToInvestorsMetadata
+    {
+        [Key]
+        public int DepositID { get; set; }
+        public bool IsDelete { get; set; }
+        [Display(Name = "پرداخت شده")]
+        public bool IsPaid { get; set; }
+        public int? Plan_id { get; set; }
+        [Display(Name = "نوع واریز")]
+        public int? DepositType_id { get; set; }
+        [Display(Name = "درصد واریز")]
+        public double? YieldPercent { get; set; }
+        [Display(Name = "توضیحات")]
+        public string Description { get; set; }
+        [Display(Name = "تاریخ واریز")]
+        public DateTime? DepositDate { get; set; }
+        public int? CreateUser_id { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? PaidDate { get; set; }
+        public DateTime? EditDate { get; set; }
+        public int? DeleteUser_id { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        [Display(Name = "کل مبلغ واریزی")]
+        public long? TotalDeposit { get; set; }
+
+
+    }
+    [MetadataType(typeof(Tbl_DepositToInvestorsMetadata))]
+    public partial class Tbl_DepositToInvestors
+    {
+
+    }
+}
---
+++ DataLayer/MetadataClasses/Tbl_DepositTypesMetadata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DataLayer
+{
+    public class Tbl_DepositTypesMetadata
+    {
+        [Key]
+        public int DepositTypeID { get; set; }
+        [Display(Name = "نوع واریز")]
+        public string DepositTypeName { get; set; }
+    }
+    [MetadataType(typeof(Tbl_DepositTypesMetadata))]
+    public partial class Tbl_DepositTypes
+    {
+
+    }
+}
