55f4ab0d526b1c92712738438b65267195405e4b
Sajad NorMohamadManesh <sajad.normohamadmanesh@gmail.com>
2021-12-07 16:16:14 +0330

    Refactor AccountController views

---
+++ HamAfarin/Content/js/views/account/account.js
@@ -0,0 +1,33 @@
+﻿var timer2 = "2:01";
+
+var interval = setInterval(function () {
+    var timer = timer2.split(':');
+    //by parsing integer, I avoid all extra string processing
+    var minutes = parseInt(timer[0], 10);
+    var seconds = parseInt(timer[1], 10);
+    --seconds;
+    minutes = (seconds < 0) ? --minutes : minutes;
+    if (minutes < 0) clearInterval(interval);
+    seconds = (seconds < 0) ? 59 : seconds;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+    //minutes = (minutes < 10) ?  minutes : minutes;
+    $('.countdown').html(minutes + ':' + seconds);
+    timer2 = minutes + ':' + seconds;
+    if (seconds == 0 && minutes == 0) {
+        $('#timeSms').hide();
+        $('#SendSms').css("display", "");
+    }
+}, 1000);
+
+function SendSms(id) {
+    $('#SendSms').hide();
+    $.ajax({
+        url: "/Account/SendSms/" + id,
+        type: "Get"
+    }).done(function (resault) {
+        alert(resault.Message);
+        location.reload();
+        $('#timeSms').css("display", "");
+        $('#SendSms').hide();
+    });
+}
