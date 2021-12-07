783669df99e1a85033fc25560fffe27d7681946d
Sajad NorMohamadManesh <sajad.normohamadmanesh@gmail.com>
2021-12-07 18:06:43 +0330

    Refactor InvestmentProcess js

---
+++ HamAfarin/Content/js/views/investment-process/investment-process.js
@@ -0,0 +1,12 @@
+﻿function GoToElement(elementId) {
+
+    //document.getElementById("header" + elementId).scrollIntoView();
+
+    $(document).ready(function () {
+
+        $('html, body').animate({
+            scrollTop: $("#header" + elementId).offset().top
+        }, 2000);
+    });
+    //location.href("#"+elementId);
+}
