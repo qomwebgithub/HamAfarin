05d41201de8576d85d2fc4182f91c7afa3a88658
Sajad NorMohamadManesh <sajad.normohamadmanesh@gmail.com>
2021-12-08 12:42:40 +0330

    Refactor BusinessPlans js

---
+++ HamAfarin/Content/js/views/business-plans/index.js
@@ -0,0 +1,133 @@
+﻿var pageIndex = 1;
+var pageSize = 6;
+var totalPageCount = 1;
+var searchTextInput = "";
+
+if ($(window).data('ajaxready') == true) {
+    appendData();
+}
+
+//.data('ajaxready', true) این مقدار برای جلوگیری ارسال درخواست تکراری استفاده شده است
+$(window).data('ajaxready', true).scroll(function () {
+
+    if ($(window).data('ajaxready') == false) return;
+
+    var footer = $('#risk');
+
+    if (totalPageCount >= pageIndex &&
+        $(window).scrollTop() + $(window).height() >= $(footer).offset().top) {
+        appendData();
+    }
+});
+
+// ارسال اولین درخواست هنگام لود صفحه بدون اسکرول
+if ($(window).data('ajaxready') == true) {
+    appendData();
+}
+
+// درخواست ایجکس
+function appendData() {
+    $(window).data('ajaxready', false);
+    $.ajax({
+        type: 'GET',
+        url: '/BusinessPlans/ActivePlans',
+        data: { "searchText": searchTextInput, "lowestPrice": minFilterPriceInput, "highestPrice": maxFilterPriceInput, "page": pageIndex },
+        success: function (data) {
+            if (data != null) {
+                $("#list-plans").append(data);
+                totalPageCount = Math.ceil(parseInt($(".hidden-input-paging").val()) / pageSize);
+                pageIndex++;
+            }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert("appendData, " + xhr.status + ", " + thrownError);
+        },
+        beforeSend: function () {
+            $("#progress").show();
+        },
+        complete: function () {
+            $("#progress").hide();
+            $(window).data('ajaxready', true);
+        },
+    });
+}
+
+function filterPriceInput() {
+    var min = $('.min-value').val();
+    var max = $('.max-value').val();
+
+    if (min != "" && max != "") {
+        maxFilterPriceInput = min;
+        minFilterPriceInput = max;
+    }
+}
+
+// جستجو ایجکس
+function search() {
+    pageIndex = 1;
+    $("#list-plans").empty();
+    filterPriceInput();
+    searchTextInput = $('#search_box').val();
+    appendData();
+}
+
+$("#search_box").on('keyup', function (e) {
+    if (e.which == 13) {
+        search();
+    }
+});
+
+//range silder
+; (function () {
+
+    var doubleHandleSlider = document.querySelector('.double-handle-slider');
+    var minValInput = document.querySelector('.min-value');
+    var maxValInput = document.querySelector('.max-value');
+
+
+    noUiSlider.create(doubleHandleSlider, {
+        start: [lowestPrice, highestPrice],
+        connect: true,
+        tooltips: true,
+        step: (highestPrice - lowestPrice) / 100,
+        range: {
+            'min': [lowestPrice],
+            'max': [highestPrice]
+        },
+        format: {
+            to: function (value) {
+                return value;
+            },
+            from: function (value) {
+                return value;
+            }
+        }
+    });
+
+    // can also be on 'update' for instant update
+    doubleHandleSlider.noUiSlider.on('change', function (values, handle) {
+
+        // This version updates both inputs.
+        var rangeValues = values;
+        minValInput.value = rangeValues[0];
+        maxValInput.value = rangeValues[1];
+
+        /*
+                // This version updates a single input on change
+                var val = values[handle]; // 0 or 1
+
+                if(handle) {
+                    maxValInput.value = Math.round(val);
+                } else {
+                    minValInput.value = Math.round(val);
+                }*/
+    });
+
+    minValInput.addEventListener('change', function () {
+        doubleHandleSlider.noUiSlider.set([this.value, null]);
+    });
+
+    maxValInput.addEventListener('change', function () {
+        doubleHandleSlider.noUiSlider.set([null, this.value]);
+    });
+})();
---
+++ HamAfarin/Content/js/views/business-plans/single-business-plan.js
@@ -0,0 +1,86 @@
+﻿$(document).ready(function () {
+    $(window).bind('scroll', function () {
+        var vPos = $(window).scrollTop();
+        var totalH = $('.stickyNav_container').offset().top;
+        var finalSize = totalH - vPos;
+        var btnSarmayeGozari = $("#btnSarmayeGozari").html();
+
+        if (finalSize <= 0) {
+            $('.stickyNav').css({
+                'position': 'fixed',
+                'top': 0
+            });
+            $('.stickyNav').addClass('w-100');
+            $("#btnSarmayeGozariInMenu").html(btnSarmayeGozari);
+        } else {
+            $('.stickyNav').css({
+                'position': 'static'
+            })
+            $('.stickyNav').removeClass('w-100');
+            $("#btnSarmayeGozariInMenu").html("");
+        }
+    });
+});
+
+function CopyToClipboard() {
+    var textToCopy = window.location.href;
+    // navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard api method'
+        alert("آدرس صفحه در کلیپ بورد شما کپی شد: " + textToCopy);
+        return navigator.clipboard.writeText(textToCopy);
+    } else {
+        // text area method
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        alert("آدرس صفحه در کلیپ بورد شما کپی شد: " + textToCopy);
+        return new Promise((res, rej) => {
+            // here the magic happens
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
+    }
+}
+
+function AlertInvestment(message, redirect) {
+    Swal.fire({
+        icon: 'warning',
+        title: 'توجه',
+        text: message,
+        confirmButtonText: `قبول`,
+        confirmButtonColor: '#d0aa45',
+    }).then((result) => {
+        if (result.isConfirmed && redirect) {
+            window.location.href = "/BusinessPlans";
+        }
+    });
+    //if (confirm(message))
+    //        window.location.href = "/BusinessPlans";
+}
+
+function ShowWarranty() {
+    $("#WarranyModal").modal();
+}
+
+
+function Success() {
+    $("#addCommentModal").modal('hide');
+    $("#addQuestionModal").modal('hide');
+}
+
+function showAlert(message) {
+    alert(message);
+}
+
+function GoToElement(elementId) {
+    $("#" + elementId).addClass("show mt-5");
+    document.getElementById("header" + elementId).scrollIntoView();
+    //location.href("#"+elementId);
+}
