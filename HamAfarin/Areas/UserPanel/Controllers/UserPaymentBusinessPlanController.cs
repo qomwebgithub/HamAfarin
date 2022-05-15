0f6b5402fa4ae396737602f8097a085548a857e7
AhmadZarei <AhmadZarei@DESKTOP-5RO5VMO>
2022-05-15 14:38:17 +0430

    تغییرات در نمایش کامنت و پرسش و پاسخ, تغییرات در نمایش سرمایه گذاری کاربر

--- HamAfarin/Areas/UserPanel/Controllers/UserPaymentBusinessPlanController.cs
+++ HamAfarin/Areas/UserPanel/Controllers/UserPaymentBusinessPlanController.cs
@@ -232,13 +232,15 @@ namespace HamAfarin.Areas.UserPanel.Controllers
             selectPayment.IsAcceptInvestment = planService.IsAcceptInvestmentPlan(db, tbl_BussinessPlans.BussinessPlanID);
 
             ViewBag.Privacy = db.Tbl_Settings.Select(s => s.Privacy).FirstOrDefault();
-            ViewBag.TotalPayment = db.Tbl_BusinessPlanPayment
+           List<Tbl_BusinessPlanPayment> qlstBusinessPlanPayment = db.Tbl_BusinessPlanPayment
                 .Where(p => p.BusinessPlan_id == qBusinessPlanPayment.BusinessPlan_id &&
                     p.PaymentUser_id == qBusinessPlanPayment.PaymentUser_id &&
                     p.IsConfirmedFromAdmin &&
-                    p.IsPaid && p.IsDelete == false)
-                .Select(p => p.PaymentPrice)
-                .Sum();
+                    p.IsPaid && p.IsDelete == false).ToList();
+
+            ViewBag.TotalPayment = qlstBusinessPlanPayment.Select(p => p.PaymentPrice).Sum();
+            ViewBag.Count = qlstBusinessPlanPayment.Count;
+
             ViewBag.DepositToInvestors = db.Tbl_DepositToInvestorsDetails.Where(p => p.InvestorUser_id == UserID &&p.Tbl_DepositToInvestors.Plan_id == tbl_BussinessPlans.BussinessPlanID).Sum(p => p.DepositAmount);
             ViewBag.Notify = notify;
 
