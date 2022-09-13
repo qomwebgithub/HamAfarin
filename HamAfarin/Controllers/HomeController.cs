52fc8de8617563913e94a637c0dcc81d64162034
Ho3ein1383 <hosseinmozaffari2004@gmail.com>
2022-09-13 14:08:48 +0430

    افزودن صفحه 404

--- HamAfarin/Controllers/HomeController.cs
+++ HamAfarin/Controllers/HomeController.cs
@@ -272,5 +272,11 @@ namespace Hamafarin.Controllers
             };
             return View(footerViewModel);
         }
+
+        [Route("404")]
+        public ActionResult Error404()
+        {
+            return View();
+        }
     }
 }
